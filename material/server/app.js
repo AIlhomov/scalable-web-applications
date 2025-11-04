e3753721ed717eb2e1ab5d75a1d283cc73e0a3a2
AIlhomov <Ilhomovvahid@gmail.com>
2025-11-04 15:31:11 +0200

    before project

--- material/server/app.js
+++ material/server/app.js
@@ -9,6 +9,8 @@ const app = new Hono();
 const sql = postgres();
 const redis = new Redis(6379, "redis");
 
+const redisProducer = new Redis(6379, "redis");
+const QUEUE_NAME = "users";
 
 app.use("*", async (c, next) => {
     c.res.headers.set("X-Replica-Id", REPLICA_ID);
@@ -19,6 +21,14 @@ app.use("/*", cors());
 app.use("/*", logger());
 
 app.get("/", (c) => c.json({ message: "Hello world!" }));
+
+app.post("/users", async (c) => {
+    const { name } = await c.req.json();
+    await redisProducer.lpush(QUEUE_NAME, JSON.stringify({ name }));
+    c.status(202);
+    return c.body("Accepted");
+});
+
 app.get("/todos", async (c) => {
     const todos = await sql`SELECT * FROM todos`;
     return c.json(todos);
